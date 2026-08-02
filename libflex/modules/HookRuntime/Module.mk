aa4739c56151025f3eb95c7bbfb384ee223b3341
euoradan <zelihmannradan@gmail.com>
2026-08-02 03:02:22 -0300

    Compile complete-image search and address-based C hooks

--- libflex/modules/HookRuntime/Module.mk
+++ libflex/modules/HookRuntime/Module.mk
@@ -5,6 +5,8 @@ ALLFLEXING_HOOK_RUNTIME_SOURCES := \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeSnapshotRegistryBridge.m \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeCrashGuards.m \
 	$(ALLFLEXING_ROOT)/FLEXDeferredApplyPolicy.m \
+	$(ALLFLEXING_ROOT)/FLEXAddressCHookEngine.m \
+	$(ALLFLEXING_ROOT)/FLEXRuntimeSearchCompleteness.m \
 	$(ALLFLEXING_ROOT)/FLEXMutableIndexSetIntersection.m \
 	$(ALLFLEXING_ROOT)/FLEXUIActionCompatibility.m \
 	$(ALLFLEXING_ROOT)/FLEXPersistenceStore.m \
