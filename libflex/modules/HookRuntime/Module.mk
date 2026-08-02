7e3421a4c5ea58190221f12098c436bf02835ff5
euoradan <zelihmannradan@gmail.com>
2026-08-02 00:34:34 -0300

    Compile image-scoped runtime session

--- libflex/modules/HookRuntime/Module.mk
+++ libflex/modules/HookRuntime/Module.mk
@@ -1,11 +1,10 @@
 ALLFLEXING_HOOK_RUNTIME_SOURCES := \
 	$(ALLFLEXING_ROOT)/FLEXABIResolver.mm \
 	$(ALLFLEXING_ROOT)/FLEXABIResolverIntegration.m \
-	$(ALLFLEXING_ROOT)/FLEXStrictABIClassification.m \
+	$(ALLFLEXING_ROOT)/FLEXRuntimeImageSession.mm \
 	$(ALLFLEXING_ROOT)/FLEXPersistenceStore.m \
 	$(ALLFLEXING_ROOT)/FLEXPersistenceIntegration.m \
 	$(ALLFLEXING_ROOT)/FLEXHookRegistry.m \
-	$(ALLFLEXING_ROOT)/FLEXRuntimeBrowserSearch.m \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeHookActions.m \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeHookIntegration.xm \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeScanner.m
