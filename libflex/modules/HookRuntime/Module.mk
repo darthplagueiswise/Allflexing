5cc89055ef93c885342c8382279d551316a5e8bc
euoradan <zelihmannradan@gmail.com>
2026-08-02 02:04:09 -0300

    Compile deferred Apply policy

--- libflex/modules/HookRuntime/Module.mk
+++ libflex/modules/HookRuntime/Module.mk
@@ -4,6 +4,7 @@ ALLFLEXING_HOOK_RUNTIME_SOURCES := \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeImageSession.mm \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeSnapshotRegistryBridge.m \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeCrashGuards.m \
+	$(ALLFLEXING_ROOT)/FLEXDeferredApplyPolicy.m \
 	$(ALLFLEXING_ROOT)/FLEXMutableIndexSetIntersection.m \
 	$(ALLFLEXING_ROOT)/FLEXUIActionCompatibility.m \
 	$(ALLFLEXING_ROOT)/FLEXPersistenceStore.m \
