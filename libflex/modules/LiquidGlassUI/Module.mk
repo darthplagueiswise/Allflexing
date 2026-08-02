f9129eaa2f94a0a2dce6b4b7222b2882aacde3cd
euoradan <zelihmannradan@gmail.com>
2026-08-01 21:38:33 -0300

    Split compact Liquid Glass UI into auditable modules

--- libflex/modules/LiquidGlassUI/Module.mk
+++ libflex/modules/LiquidGlassUI/Module.mk
@@ -1,5 +1,7 @@
 ALLFLEXING_LIQUID_GLASS_UI_SOURCES := \
-	$(ALLFLEXING_ROOT)/FLEXCompactRuntimeUI.m \
+	$(ALLFLEXING_ROOT)/FLEXCompactRuntimeCore.m \
+	$(ALLFLEXING_ROOT)/FLEXCompactRuntimeControllers.m \
+	$(ALLFLEXING_ROOT)/FLEXCompactFLEXBaseStyle.m \
 	$(ALLFLEXING_ROOT)/FLEXHookEntryDetailController.m \
 	$(ALLFLEXING_ROOT)/FLEXHookSettingsController.m \
 	$(ALLFLEXING_ROOT)/FLEXHookToggles.m \
