a70987911de7158d966b0d3dd3be1174bb90a6ca
euoradan <zelihmannradan@gmail.com>
2026-08-01 22:44:00 -0300

    Remove per-layout glass styling and use native UIKit tables

--- libflex/AllFLEXing/FLEXCompactFLEXBaseStyle.m
+++ libflex/AllFLEXing/FLEXCompactFLEXBaseStyle.m
@@ -4,143 +4,94 @@
 
 #import <objc/runtime.h>
 
-static UIFont *FLEXInternalScaledFont(CGFloat size,
-                                      UIFontWeight weight,
-                                      UIFontTextStyle style,
-                                      CGFloat maximum) {
-    UIFont *base = [UIFont systemFontOfSize:size weight:weight];
-    return [[UIFontMetrics metricsForTextStyle:style]
-        scaledFontForFont:base maximumPointSize:maximum];
-}
+__attribute__((used)) static const char kFLEXNativeUIKitRenderingMarker[] =
+    "AllFLEXing native UIKit rendering bootstrap ABI 1";
 
-static void (*FLEXBaseTableViewDidLoad)(id, SEL) = NULL;
-static void (*FLEXBaseTableViewWillAppear)(id, SEL, BOOL) = NULL;
-static void (*FLEXBaseTableViewDidLayoutSubviews)(id, SEL) = NULL;
-
-static NSInteger FLEXInternalSectionCount(UITableView *tableView) {
-    id<UITableViewDataSource> dataSource = tableView.dataSource;
-    if ([dataSource respondsToSelector:@selector(numberOfSectionsInTableView:)]) {
-        return [dataSource numberOfSectionsInTableView:tableView];
+static void FLEXExchangeClassMethods(Class cls, SEL original, SEL replacement) {
+    Method originalMethod = class_getClassMethod(cls, original);
+    Method replacementMethod = class_getClassMethod(cls, replacement);
+    if (originalMethod && replacementMethod) {
+        method_exchangeImplementations(originalMethod, replacementMethod);
     }
-    return 1;
 }
 
-static NSInteger FLEXInternalRowCount(UITableView *tableView, NSInteger section) {
-    id<UITableViewDataSource> dataSource = tableView.dataSource;
-    if ([dataSource respondsToSelector:@selector(tableView:numberOfRowsInSection:)]) {
-        return [dataSource tableView:tableView numberOfRowsInSection:section];
-    }
-    return 0;
+@implementation FLEXLiquidGlass (AllFLEXingNativeUIKitTables)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class cls = FLEXLiquidGlass.class;
+        FLEXExchangeClassMethods(cls,
+                                 @selector(styleTableView:),
+                                 @selector(af_native_styleTableView:));
+        FLEXExchangeClassMethods(cls,
+                                 @selector(styleTableCell:),
+                                 @selector(af_native_styleTableCell:));
+        FLEXExchangeClassMethods(cls,
+                                 @selector(styleSearchBar:),
+                                 @selector(af_native_styleSearchBar:));
+    });
 }
 
-static void FLEXStyleVisibleInternalCells(UITableViewController *controller) {
-    UITableView *tableView = controller.tableView;
++ (void)af_native_styleTableView:(UITableView *)tableView {
     if (!tableView) {
         return;
     }
-    FLEXConfigureCompactRuntimeTable(tableView);
-    for (UITableViewCell *cell in tableView.visibleCells) {
-        NSIndexPath *indexPath = [tableView indexPathForCell:cell];
-        if (!indexPath) {
-            continue;
-        }
-        NSInteger count = FLEXInternalRowCount(tableView, indexPath.section);
-        FLEXStyleCompactRuntimeCell(
-            cell,
-            FLEXCompactPositionForRow(indexPath.row, MAX(count, 1))
-        );
-
-        id configuration = cell.contentConfiguration;
-        if ([configuration isKindOfClass:UIListContentConfiguration.class]) {
-            UIListContentConfiguration *content =
-                [(UIListContentConfiguration *)configuration copy];
-            content.textProperties.font = FLEXInternalScaledFont(
-                13.5,
-                UIFontWeightMedium,
-                UIFontTextStyleBody,
-                17.0
-            );
-            content.secondaryTextProperties.font = FLEXInternalScaledFont(
-                10.5,
-                UIFontWeightRegular,
-                UIFontTextStyleCaption1,
-                13.0
-            );
-            content.secondaryTextProperties.numberOfLines = 2;
-            content.directionalLayoutMargins = NSDirectionalEdgeInsetsMake(
-                7.0, 14.0, 7.0, 12.0);
-            cell.contentConfiguration = content;
-        } else {
-            cell.textLabel.font = FLEXInternalScaledFont(
-                13.5,
-                UIFontWeightMedium,
-                UIFontTextStyleBody,
-                17.0
-            );
-            cell.detailTextLabel.font = FLEXInternalScaledFont(
-                10.5,
-                UIFontWeightRegular,
-                UIFontTextStyleCaption1,
-                13.0
-            );
-        }
-    }
-
-    NSInteger sections = FLEXInternalSectionCount(tableView);
-    for (NSInteger section = 0; section < sections; section++) {
-        UITableViewHeaderFooterView *header = [tableView headerViewForSection:section];
-        header.textLabel.font = FLEXInternalScaledFont(
-            12.0,
-            UIFontWeightSemibold,
-            UIFontTextStyleSubheadline,
-            14.5
-        );
-        header.textLabel.textColor = UIColor.secondaryLabelColor;
-        UITableViewHeaderFooterView *footer = [tableView footerViewForSection:section];
-        footer.textLabel.font = FLEXInternalScaledFont(
-            10.5,
-            UIFontWeightRegular,
-            UIFontTextStyleCaption1,
-            13.0
-        );
-    }
-
-    UISearchController *search = controller.navigationItem.searchController;
-    if (search) {
-        search.searchBar.searchTextField.font = FLEXInternalScaledFont(
-            13.5,
-            UIFontWeightRegular,
-            UIFontTextStyleBody,
-            16.0
-        );
-        [FLEXLiquidGlass styleSearchBar:search.searchBar];
-    }
+    tableView.backgroundColor = UIColor.systemGroupedBackgroundColor;
+    tableView.opaque = YES;
+    tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    tableView.separatorColor = UIColor.separatorColor;
+    tableView.cellLayoutMarginsFollowReadableWidth = YES;
+    tableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+    tableView.sectionHeaderTopPadding = 8.0;
 }
+
++ (void)af_native_styleTableCell:(UITableViewCell *)cell {
+    if (!cell) {
+        return;
+    }
+    cell.backgroundView = nil;
+    cell.selectedBackgroundView = nil;
+    cell.backgroundColor = UIColor.secondarySystemGroupedBackgroundColor;
+    cell.contentView.backgroundColor = UIColor.clearColor;
+    cell.preservesSuperviewLayoutMargins = YES;
+}
+
++ (void)af_native_styleSearchBar:(UISearchBar *)searchBar {
+    if (!searchBar) {
+        return;
+    }
+    searchBar.searchBarStyle = UISearchBarStyleDefault;
+    searchBar.backgroundImage = nil;
+    UISearchTextField *field = searchBar.searchTextField;
+    field.backgroundColor = nil;
+    field.background = nil;
+    field.borderStyle = UITextBorderStyleRoundedRect;
+}
+
+@end
+
+static void (*FLEXBaseTableViewDidLoad)(id, SEL) = NULL;
 
 static void FLEXBaseStyledViewDidLoad(id self, SEL _cmd) {
     if (FLEXBaseTableViewDidLoad) {
         FLEXBaseTableViewDidLoad(self, _cmd);
     }
-    UITableViewController *controller = self;
-    controller.navigationItem.largeTitleDisplayMode = UINavigationItemLargeTitleDisplayModeNever;
-    [FLEXLiquidGlass applyToViewController:controller];
-    FLEXStyleVisibleInternalCells(controller);
-}
 
-static void FLEXBaseStyledViewWillAppear(id self, SEL _cmd, BOOL animated) {
-    if (FLEXBaseTableViewWillAppear) {
-        FLEXBaseTableViewWillAppear(self, _cmd, animated);
-    }
     UITableViewController *controller = self;
-    [FLEXLiquidGlass applyToViewController:controller];
-    FLEXStyleVisibleInternalCells(controller);
-}
-
-static void FLEXBaseStyledViewDidLayoutSubviews(id self, SEL _cmd) {
-    if (FLEXBaseTableViewDidLayoutSubviews) {
-        FLEXBaseTableViewDidLayoutSubviews(self, _cmd);
+    UITableView *tableView = controller.tableView;
+    if (!tableView) {
+        return;
     }
-    FLEXStyleVisibleInternalCells((UITableViewController *)self);
+
+    tableView.backgroundColor = UIColor.systemGroupedBackgroundColor;
+    tableView.opaque = YES;
+    tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    tableView.separatorColor = UIColor.separatorColor;
+    tableView.cellLayoutMarginsFollowReadableWidth = YES;
+    tableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+    tableView.sectionHeaderTopPadding = 8.0;
+    tableView.rowHeight = UITableViewAutomaticDimension;
 }
 
 static void FLEXInstallClassOverride(Class cls,
@@ -175,18 +126,6 @@ static void FLEXInstallClassOverride(Class cls,
             @selector(viewDidLoad),
             (IMP)FLEXBaseStyledViewDidLoad,
             (IMP *)&FLEXBaseTableViewDidLoad
-        );
-        FLEXInstallClassOverride(
-            base,
-            @selector(viewWillAppear:),
-            (IMP)FLEXBaseStyledViewWillAppear,
-            (IMP *)&FLEXBaseTableViewWillAppear
-        );
-        FLEXInstallClassOverride(
-            base,
-            @selector(viewDidLayoutSubviews),
-            (IMP)FLEXBaseStyledViewDidLayoutSubviews,
-            (IMP *)&FLEXBaseTableViewDidLayoutSubviews
         );
     });
 }
