0d658a7c1fa998e6e0bb1c2928511fc7967deaeb
euoradan <zelihmannradan@gmail.com>
2026-08-03 23:49:15 -0300

    Remove collapsed persistence and scanner layers from manifest

--- libflex/modules/HookRuntime/Module.mk
+++ libflex/modules/HookRuntime/Module.mk
@@ -10,11 +10,9 @@ ALLFLEXING_HOOK_RUNTIME_SOURCES := \
 	$(ALLFLEXING_ROOT)/FLEXAddressCHookEngine.m \
 	$(ALLFLEXING_ROOT)/FLEXAddressCHookResolverAPI.m \
 	$(ALLFLEXING_ROOT)/FLEXASLRAddressRebase.m \
-	$(ALLFLEXING_ROOT)/FLEXStrictObjectiveCRuntimeScanner.m \
 	$(ALLFLEXING_ROOT)/FLEXMutableIndexSetIntersection.m \
 	$(ALLFLEXING_ROOT)/FLEXUIActionCompatibility.m \
 	$(ALLFLEXING_ROOT)/FLEXPersistenceStore.m \
-	$(ALLFLEXING_ROOT)/FLEXPersistenceIntegration.m \
 	$(ALLFLEXING_ROOT)/FLEXHookRegistry.m \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeHookActions.m \
 	$(ALLFLEXING_ROOT)/FLEXRuntimeHookIntegration.xm \
