4ec3a097f0804a78052826f928ae0cda7a99baf3
euoradan <zelihmannradan@gmail.com>
2026-08-04 00:36:47 -0300

    Remove compact controller swizzle layer from manifest

--- libflex/modules/LiquidGlassUI/Module.mk
+++ libflex/modules/LiquidGlassUI/Module.mk
@@ -1,6 +1,5 @@
 ALLFLEXING_LIQUID_GLASS_UI_SOURCES := \
 	$(ALLFLEXING_ROOT)/FLEXCompactRuntimeCore.m \
-	$(ALLFLEXING_ROOT)/FLEXCompactRuntimeControllers.m \
 	$(ALLFLEXING_ROOT)/FLEXCompactFLEXBaseStyle.m \
 	$(ALLFLEXING_ROOT)/FLEXHookEntryDetailController.m \
 	$(ALLFLEXING_ROOT)/FLEXHookSettingsController.m \
